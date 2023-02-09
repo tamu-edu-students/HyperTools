c6e3aa34dd1e4d7dcb4af21361051a2a5b22e205
antmedellin <90658983+antmedellin@users.noreply.github.com>
2023-02-08 23:29:09 -0600

    Update hyperfunctions.h

--- hyperfunctions.h
+++ hyperfunctions.h
@@ -9,6 +9,7 @@
 using namespace std;
 using namespace cv;
 
+// functions that are used with the thread pool
 void SAM_img_Child(int id, int k, vector<Mat>* mlt1, vector<vector<int>>* reference_spectrums,Mat* spec_simil_img,int* ref_spec_index);   
 void SID_img_Child(int id, int k, vector<Mat>* mlt2, vector<vector<int>>* reference_spectrums2,Mat* spec_simil_img,int* ref_spec_index);
 void SCM_img_Child(int id, int k, vector<Mat>* mlt2, vector<vector<int>>* reference_spectrums2,Mat* spec_simil_img,int* ref_spec_index);
@@ -36,7 +37,6 @@ public:
 	vector<vector<int>> reference_spectrums;
 	vector<Vec3b> reference_colors;
 
-
 	double polygon_approx_coeff=0;
 	double avgDist=35;
 	double fov=35;
@@ -59,15 +59,14 @@ public:
 	string camera_database="../json/camera_database.json";
 	string output_polygons="../json/file.json";
 
-
-	// need to error handle all of the functions
-
+	// functions to load differnt types of images
 	void LoadImageHyper1(string file_name);
 	void LoadImageHyper2(string file_name);
 	void LoadImageClassified(string file_name);
 	void LoadFeatureImage1(string file_name);
 	void LoadFeatureImage2(string file_name);
 
+	//functions to display different types of images
 	void DispClassifiedImage();
 	void DispEdgeImage();
 	void DispDifference();
@@ -75,40 +74,31 @@ public:
 	void DispTiled();
 	void DispFalseImage();
 	void DispSpecSim();
+	void DispFeatureImgs(); 
 
-
+	// various capabilities
 	void DetectContours();
 	void DifferenceOfImages();
 	void EdgeDetection();
 	void GenerateFalseImg();
-
-	// wip
-	void SAM_img();
+	void TileImage(); // set for 164 needs to be made modular 
+	void FeatureExtraction();  
+	void FeatureTransformation(); 
 	
+	// functions involving spectral similarity algorithms
+	void SAM_img();
 	void SID_img();
 	void SCM_img();
 	void SemanticSegmenter();
 	void SpecSimilParent();
-	//void SpecSimilChild(int id, int col);  // right now separated due to threadpool,  but need to incorporate
-	// wip
 
-
-
-	void DispFeatureImgs(); // need to create another one for matched features
-	void FeatureExtraction();  // currently displays matches, need to change
-	void FeatureTransformation(); // verify what inlier accuracy means
-	void TileImage(); // set for 164 needs to be made modular 
-
-
-	// need to clean up below
+	//functions involving json files
 	void read_spectral_json(string file_name);
 	void writeJSON(Json::Value &event, vector<vector<Point> > &contours, int idx, string classification, int count);
 	void read_img_json(string file_name);
 	void save_ref_spec_json(string file_name); // save real spectrum
 	void read_ref_spec_json(string file_name);
 	void save_new_spec_database_json(string file_name);
-
-
 
 };
 
