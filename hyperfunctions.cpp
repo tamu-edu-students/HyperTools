f439528ac83030defea261debd1d4aa043558628
Albert Ma <albert.ma.252@gmail.com>
2023-03-06 15:35:22 -0600

    new documentation 3/6/2023

--- hyperfunctions.cpp
+++ hyperfunctions.cpp
@@ -301,7 +301,12 @@ void HyperFunctions::DifferenceOfImages()
     difference_img = output_image; 
 }
               
-                  
+//---------------------------------------------------------
+// Name: EdgeDetection
+// Description: to detect edges or boundaries between different regions or objects in an image
+// PreCondition: an output image prepared for edge detection
+// PostCondition: edge_image set to the modigfied output image after edge detection 
+//---------------------------------------------------------                  
 void HyperFunctions::EdgeDetection( )
 {
 	// create a copy of the incoming image in terms of size (length and width) and initialize as an all black image
@@ -502,7 +507,6 @@ void HyperFunctions::DetectContours()
 } // end function
 
 
-// Creates tile image or default/base image
 void HyperFunctions::TileImage()
 {
     //vector<Mat> mlt1=*mlt2; 
