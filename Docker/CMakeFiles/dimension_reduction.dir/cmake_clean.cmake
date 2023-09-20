090285ef3feb4ac9b3dcfd69832a176469931e56
isaiahmunoz2002-tamu <isaiahmunoz2002@tamu.edu>
2023-09-20 22:40:33 +0000

    cosine still doesn't work

---
+++ Docker/CMakeFiles/dimension_reduction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dimension_reduction.dir/examples/dimension_reduction.cpp.o"
+  "dimension_reduction"
+  "dimension_reduction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dimension_reduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Docker/CMakeFiles/ground_truth_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ground_truth_example.dir/examples/ground_truth_example.cpp.o"
+  "ground_truth_example"
+  "ground_truth_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ground_truth_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
