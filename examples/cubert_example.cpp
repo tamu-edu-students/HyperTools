e8240428c2fab1ef48ef6daa3ea947c54091328c
Anthony <antmedellin@gmail.com>
2023-10-12 19:55:00 +0000

    example update

--- examples/cubert_example.cpp
+++ examples/cubert_example.cpp
@@ -14,18 +14,26 @@ int main (int argc, char *argv[])
 
     HyperFunctionsCuvis HyperFunctions1;
 
-    HyperFunctions1.cubert_img = "../../HyperImages/cornfields/session_002/session_002_490.cu3";
-    HyperFunctions1.dark_img = "../../HyperImages/cornfields/Calibration/dark__session_002_003_snapshot16423119279414228.cu3";
-    HyperFunctions1.white_img = "../../HyperImages/cornfields/Calibration/white__session_002_752_snapshot16423136896447489.cu3";
-    HyperFunctions1.dist_img = "../../HyperImages/cornfields/Calibration/distanceCalib__session_000_790_snapshot16423004058237746.cu3";
+    // HyperFunctions1.cubert_img = "../../HyperImages/cornfields/session_002/session_002_490.cu3";
+    // HyperFunctions1.dark_img = "../../HyperImages/cornfields/Calibration/dark__session_002_003_snapshot16423119279414228.cu3";
+    // HyperFunctions1.white_img = "../../HyperImages/cornfields/Calibration/white__session_002_752_snapshot16423136896447489.cu3";
+    // HyperFunctions1.dist_img = "../../HyperImages/cornfields/Calibration/distanceCalib__session_000_790_snapshot16423004058237746.cu3";
 
-    HyperFunctions1.cubert_img = "/workspaces/HyperImages/export/Auto_001.cu3s";
+    HyperFunctions1.dark_img = "../../HyperImages/Calib2/Dark.cu3s";
+    HyperFunctions1.white_img = "../../HyperImages/Calib2/White.cu3s";
+    HyperFunctions1.dist_img = "../../HyperImages/Calib2/Dist.cu3s";
+
+
+    HyperFunctions1.cubert_img = "../../HyperImages/export/Auto_001.cu3s";
+    // HyperFunctions1.cubert_img = "../../HyperImages/export/Ref/Auto_001.cu3s";
+
     HyperFunctions1.cubert_settings="../settings/ultris5";  //camera settings file 
-    HyperFunctions1.factor_dir="/workspaces/HyperTools/settings/ultris5"; // requires init.daq file
+    HyperFunctions1.factor_dir="../settings/ultris5"; // requires init.daq file
 
     
     // string file_name, const int exposure_ms, const int num_image
-    // HyperFunctions1.TakeImageHyper1("qferg",20, 1);
+    HyperFunctions1.TakeImageHyper1("qferg",50, 1);
+    // leads to a seg fault so will need to fix above
     
     
     //below only works for  ultris20 images due to different number of layers in default spectral database
@@ -34,13 +42,18 @@ int main (int argc, char *argv[])
     // HyperFunctions1.DispClassifiedImage();
 
 
-    HyperFunctions1.LoadImageHyper(HyperFunctions1.cubert_img);
-    HyperFunctions1.false_img_b=10;
-    HyperFunctions1.false_img_g=20;
-    HyperFunctions1.false_img_r=30;
-    HyperFunctions1.GenerateFalseImg();
-    HyperFunctions1.DispFalseImage();
+    //  HyperFunctions1.LoadImageHyper(HyperFunctions1.cubert_img);
+    
+    // HyperFunctions1.ReprocessCu3s();    
+    // HyperFunctions1.false_img_b=10;
+    // HyperFunctions1.false_img_g=20;
+    // HyperFunctions1.false_img_r=30;
+    // HyperFunctions1.GenerateFalseImg();
+    // // HyperFunctions1.DispFalseImage();
 
+    // cv::imwrite(HyperFunctions1.output_dir+"test_img.png", HyperFunctions1.false_img);
+
+    
     cv::waitKey();
 
 
--- src/hypercuvisfunctions.cpp
+++ src/hypercuvisfunctions.cpp
@@ -307,3 +307,122 @@ void HyperFunctionsCuvis::ExportTiff()
     
 
 }
+
+
+void HyperFunctionsCuvis::ReprocessCu3s(){
+ 
+
+  char* const userSettingsDir = const_cast<char*>(cubert_settings.c_str());
+  char* const measurementLoc = const_cast<char*>(cubert_img.c_str());
+  char* const darkLoc = const_cast<char*>(dark_img.c_str());
+  char* const whiteLoc = const_cast<char*>(white_img.c_str());
+  char* const distanceLoc = const_cast<char*>(dist_img.c_str());
+  char* const outDir = const_cast<char*>(output_dir.c_str());
+
+  std::cout << "Example 02 reprocess measurement cpp " << std::endl;
+  std::cout << "User Settings Dir: " << userSettingsDir << std::endl;
+  std::cout << "measurement file (.cu3s): " << measurementLoc << std::endl;
+  std::cout << "dark file (.cu3s): " << darkLoc << std::endl;
+  std::cout << "white file (.cu3s): " << whiteLoc << std::endl;
+  std::cout << "distance file (.cu3s): " << distanceLoc << std::endl;
+  std::cout << "output Dir: " << outDir << std::endl;
+
+  std::cout << "loading settings... " << std::endl;
+  cuvis::General::init(userSettingsDir);
+  cuvis::General::set_log_level(loglevel_info);
+
+  std::cout << "loading measurement... " << std::endl;
+  cuvis::SessionFile sessMesu(measurementLoc);
+  
+  auto optMesu = sessMesu.get_mesu(0);
+  assert(optMesu.has_value());
+  cuvis::Measurement mesu = optMesu.value();
+
+
+  std::cout << "loading dark... " << std::endl;
+  cuvis::SessionFile sessDark(darkLoc);
+  auto optDark = sessDark.get_mesu(0);
+  assert(optDark.has_value());
+  cuvis::Measurement dark = optDark.value();
+
+
+  std::cout << "loading white... " << std::endl;
+  cuvis::SessionFile sessWhite(whiteLoc);
+  auto optWhite = sessWhite.get_mesu(0);
+  assert(optWhite.has_value());
+  cuvis::Measurement white = optWhite.value();
+  std::cout << "loading distance... " << std::endl;
+  cuvis::SessionFile sessDistance(distanceLoc);
+  auto optDistance = sessDistance.get_mesu(0);
+  assert(optDistance.has_value());
+  cuvis::Measurement distance = optDistance.value();
+  std::cout << "Data 1" << mesu.get_meta()->name << " "
+            << "t=" << mesu.get_meta()->integration_time << " ms "
+            << "mode=" << mesu.get_meta()->processing_mode << " " << std::endl;
+
+  std::cout << "Loading processing context" << std::endl;
+  cuvis::ProcessingContext proc(sessMesu);
+
+  std::cout << "Set references" << std::endl;
+
+  proc.set_reference(dark, cuvis::reference_type_t::Reference_Dark);
+  proc.set_reference(white, cuvis::reference_type_t::Reference_White);
+  proc.set_reference(distance, cuvis::reference_type_t::Reference_Distance);
+
+  cuvis::ProcessingArgs procArgs;
+  cuvis::SaveArgs saveArgs;
+  saveArgs.allow_overwrite = true;
+  saveArgs.allow_session_file = true;
+  saveArgs.allow_info_file = false;
+
+  std::map<std::string, cuvis::processing_mode_t> target_modes = {
+    //   {"Raw", cuvis::processing_mode_t::Cube_Raw},
+    //   {"DS", cuvis::processing_mode_t::Cube_DarkSubtract},
+      {"Ref", cuvis::processing_mode_t::Cube_Reflectance}
+    //   {"RAD", cuvis::processing_mode_t::Cube_SpectralRadiance}
+    };
+
+  for (auto const& mode : target_modes)
+  {
+    procArgs.processing_mode = mode.second;
+    if (proc.is_capable(mesu, procArgs))
+    {
+      std::cout << "processing to mode " << mode.first << std::endl;
+      proc.set_processingArgs(procArgs);
+      proc.apply(mesu);
+      saveArgs.export_dir = std::filesystem::path(outDir) / mode.first;
+
+    //   cuvis::CubeExporter exporter(saveArgs);
+    //   exporter.apply(mesu);
+    }
+    else
+    {
+      std::cout << "cannot process to mode " << mode.first << std::endl;
+    }
+  }
+
+  
+    auto const& cube_it = mesu.get_imdata()->find(CUVIS_MESU_CUBE_KEY);
+    assert(
+        cube_it != mesu.get_imdata()->end() &&
+        "Cube not found");
+    auto cube = std::get<cuvis::image_t<std::uint16_t>>(cube_it->second);
+ cv::Mat img(
+    cv::Size(cube._width, cube._height),
+    CV_16UC(cube._channels),
+    const_cast<void*>(reinterpret_cast<const void*>(cube._data)),
+    cv::Mat::AUTO_STEP);
+
+    for (int i=0;i<img.channels();i++)
+    {
+        cv::Mat singleChannel;
+        cv::extractChannel(
+        img, singleChannel, i); 
+        singleChannel.convertTo(singleChannel, CV_8U, 1 / 16.0);
+        mlt1.push_back(singleChannel);
+        // cv::imshow(" Individual channel ", singleChannel);
+        // cv::waitKey(50);
+    } 
+
+  std::cout << "finished." << std::endl;
+}
