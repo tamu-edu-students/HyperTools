52c06485d30f0d0b403045b29acadadd5cc8bdb6
Anthony <antmedellin@gmail.com>
2023-11-10 03:58:09 +0000

    lightglue with hyperspectral images

--- examples/light_glue.py
+++ examples/light_glue.py
@@ -10,18 +10,50 @@ import os
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+import matplotlib
+matplotlib.use('TkAgg')
 
 # SuperPoint+LightGlue
-extractor = SuperPoint(max_num_keypoints=2048).eval().cuda()  # load the extractor
-matcher = LightGlue(features='superpoint').eval().cuda()  # load the matcher
+# extractor = SuperPoint(max_num_keypoints=2048).eval().cuda()  # load the extractor
+# matcher = LightGlue(features='superpoint').eval().cuda()  # load the matcher
 
 # or DISK+LightGlue
-#extractor = DISK(max_num_keypoints=2048).eval().cuda()  # load the extractor
-#matcher = LightGlue(features='disk').eval().cuda()  # load the matcher
+extractor = DISK(max_num_keypoints=2048).eval().cuda()  # load the extractor
+matcher = LightGlue(features='disk').eval().cuda()  # load the matcher
 
 # load each image as a torch.Tensor on GPU with shape (3,H,W), normalized in [0,1]
-image0 = load_image('submodules/LightGlue/assets/sacre_coeur1.jpg').cuda()
-image1 = load_image('submodules/LightGlue/assets/sacre_coeur2.jpg').cuda()
+# image0 = load_image('submodules/LightGlue/assets/sacre_coeur1.jpg').cuda()
+# image1 = load_image('submodules/LightGlue/assets/sacre_coeur2.jpg').cuda()
+# image0 = load_image('images/pan1.tiff').cuda()
+# image1 = load_image('images/pan2.tiff').cuda()
+
+
+
+#below is a way to load hyperspectral images that are tiff files
+mylist = []
+loaded,mylist = cv2.imreadmulti(mats = mylist, filename = "../HyperImages/img1.tiff", flags = cv2.IMREAD_ANYCOLOR )
+cube1=np.array(mylist)
+cube1 = cube1[:, :, :]  
+print(cube1.shape)
+
+mylist = []
+loaded,mylist = cv2.imreadmulti(mats = mylist, filename = "../HyperImages/img2.tiff", flags = cv2.IMREAD_ANYCOLOR )
+cube2=np.array(mylist)
+cube2 = cube2[:, :, :]  
+print(cube2.shape)
+
+
+image0 = cube1[100, :, :]
+# image0 = np.expand_dims(image0, axis=0)
+# print(image0.shape)
+
+image1 = cube2[100, :, :]
+# image1 = np.expand_dims(image1, axis=0)
+
+image0 = torch.from_numpy(image0).unsqueeze(0).float().cuda()
+image1 = torch.from_numpy(image1).unsqueeze(0).float().cuda()
+
 
 # extract local features
 feats0 = extractor.extract(image0)  # auto-resize the image, disable with resize=None
@@ -38,3 +70,4 @@ points1 = feats1['keypoints'][matches[..., 1]]  # coordinates in image #1, shape
 plot_images([image0.cpu(), image1.cpu()])
 #plot_keypoints(points0, points1)
 plot_matches(points0[:, :], points1[:, :])
+plt.show()
