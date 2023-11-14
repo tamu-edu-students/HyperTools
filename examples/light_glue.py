f98efcde7555b301fafd28931bbdb17bb78bd7f5
Anthony <antmedellin@gmail.com>
2023-11-14 22:27:14 +0000

    added cv2 comparison to lightglue

--- examples/light_glue.py
+++ examples/light_glue.py
@@ -14,11 +14,11 @@ import cv2
 import matplotlib
 matplotlib.use('TkAgg')
 
-# SuperPoint+LightGlue
+# SuperPoint+LightGlue # not good for hyperspectral images not many matched points neither is sift or aliked
 # extractor = SuperPoint(max_num_keypoints=2048).eval().cuda()  # load the extractor
 # matcher = LightGlue(features='superpoint').eval().cuda()  # load the matcher
 
-# or DISK+LightGlue
+# or DISK+LightGlue # getting decent results for hyperspectral images
 extractor = DISK(max_num_keypoints=2048).eval().cuda()  # load the extractor
 matcher = LightGlue(features='disk').eval().cuda()  # load the matcher
 
@@ -35,13 +35,13 @@ mylist = []
 loaded,mylist = cv2.imreadmulti(mats = mylist, filename = "../HyperImages/img1.tiff", flags = cv2.IMREAD_ANYCOLOR )
 cube1=np.array(mylist)
 cube1 = cube1[:, :, :]  
-print(cube1.shape)
+# print(cube1.shape)
 
 mylist = []
 loaded,mylist = cv2.imreadmulti(mats = mylist, filename = "../HyperImages/img2.tiff", flags = cv2.IMREAD_ANYCOLOR )
 cube2=np.array(mylist)
 cube2 = cube2[:, :, :]  
-print(cube2.shape)
+# print(cube2.shape)
 
 
 image0 = cube1[100, :, :]
@@ -58,7 +58,7 @@ image1 = torch.from_numpy(image1).unsqueeze(0).float().cuda()
 # extract local features
 feats0 = extractor.extract(image0)  # auto-resize the image, disable with resize=None
 feats1 = extractor.extract(image1)
-
+# print(feats0) # this is a dictionary with keys 'keypoints' and 'descriptors'   and keypoint scores and other information about the image, device
 # match the features
 matches01 = matcher({'image0': feats0, 'image1': feats1})
 feats0, feats1, matches01 = [rbd(x) for x in [feats0, feats1, matches01]]  # remove batch dimension
@@ -71,3 +71,39 @@ plot_images([image0.cpu(), image1.cpu()])
 #plot_keypoints(points0, points1)
 plot_matches(points0[:, :], points1[:, :])
 plt.show()
+
+
+# tradtional way with opencv
+# img1 = cube1[100, :, :]
+# img2 = cube2[100, :, :]
+# # Initiate SIFT detector
+# sift = cv2.SIFT_create()
+# # find the keypoints and descriptors with SIFT
+# kp1, des1 = sift.detectAndCompute(img1,None)
+# kp2, des2 = sift.detectAndCompute(img2,None)
+# # BFMatcher with default params
+# bf = cv2.BFMatcher()
+# matches = bf.knnMatch(des1,des2,k=2)
+# # Apply ratio test
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+# # cv.drawMatchesKnn expects list of lists as matches.
+# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# plt.imshow(img3),plt.show()
+
+# # Initiate ORB detector
+# orb = cv2.ORB_create()
+# # find the keypoints and descriptors with ORB
+# kp1, des1 = orb.detectAndCompute(cube1[100, :, :],None)
+# kp2, des2 = orb.detectAndCompute(cube2[100, :, :],None)
+# # create BFMatcher object
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+# # Match descriptors.
+# matches = bf.match(des1,des2)
+# Sort them in the order of their distance.
+# matches = sorted(matches, key = lambda x:x.distance)
+# # Draw first 10 matches.
+# img3 = cv2.drawMatches(cube1[100, :, :],kp1,cube2[100, :, :],kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# plt.imshow(img3),plt.show()
