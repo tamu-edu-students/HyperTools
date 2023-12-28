952eb5fb3c39774d88eb28c853245ae86fcafd8d
Anthony <antmedellin@gmail.com>
2023-12-28 07:18:09 +0000

    initial commit

--- examples/feature_testing.cpp
+++ examples/feature_testing.cpp
@@ -7,13 +7,75 @@
 using namespace cv;
 using namespace std;
 
+std::vector<cv::Mat> loadMatlabFile(const std::string& filename)
+{
+    std::vector<cv::Mat> mats;
+
+    // Open the MATLAB file
+    mat_t *matfp = Mat_Open(filename.c_str(), MAT_ACC_RDONLY);
+    if (matfp == NULL) {
+        std::cerr << "Error opening MATLAB file " << filename << std::endl;
+        return mats;
+    }
+
+    // Read each variable in the file
+    matvar_t *matvar = NULL;
+    while ((matvar = Mat_VarReadNext(matfp)) != NULL) {
+        // Convert the variable to a cv::Mat and add it to the vector
+        // This assumes that the variable is a 2D double matrix
+        if (matvar->rank == 2 && matvar->data_type == MAT_T_DOUBLE) {
+            cv::Mat mat(matvar->dims[0], matvar->dims[1], CV_64F, matvar->data);
+            mats.push_back(mat.clone());  // clone the mat because matvar->data will be freed
+        }
+
+        // Free the current MATLAB variable
+        Mat_VarFree(matvar);
+    }
+
+    // Close the MATLAB file
+    Mat_Close(matfp);
+
+    return mats;
+}
 
 int main (int argc, char *argv[])
 {
 
   HyperFunctions HyperFunctions1; 
   
-  
+
+  // load hyperspectral image that is a matlab file 
+  string file_name1="../../HyperImages/Indian_pines.mat";
+
+  vector<Mat> mats = loadMatlabFile(file_name1);
+
+//   sudo apt install libhdf5-dev libtool m4 automake
+// # Clone the matio repository
+// git clone git://git.code.sf.net/p/matio/matio
+
+// # Navigate into the cloned directory
+// cd matio
+
+// # Update submodules (for datasets used in unit tests)
+// git submodule update --init
+
+// # Generate the configure script
+// ./autogen.sh
+
+// # Configure the build
+// ./configure
+
+// # Build the library
+// make
+
+// # Run tests (optional)
+// make check
+
+// # Install the library
+// sudo make install
+
+  return -1;
+
   // load hyperspectral image
   string file_name1="../../HyperImages/img1.tiff";
   HyperFunctions1.LoadImageHyper(file_name1);
