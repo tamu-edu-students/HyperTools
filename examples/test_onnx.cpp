b081fa9da4e3a547c90825f2b00e1a7d99dc5c2f
vamin20-tamu <vamin20@tamu.edu>
2024-01-26 20:49:25 +0000

    update to dockerfile and cmake

---
+++ examples/test_onnx.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <onnxruntime/core/providers/cpu/cpu_provider_factory.h>
+#include <onnxruntime/core/providers/cpu/cpu_execution_provider.h>
+#include <onnxruntime/core/providers/providers.h>
+
+using namespace cv;
+using namespace std;
+
