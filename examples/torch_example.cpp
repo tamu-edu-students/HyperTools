84e2cb8e8f731c3ec12d44c728a5e3ef8970b13e
root <root@bcdc-5w9rm33.engr.tamu.edu>
2024-03-27 09:54:32 -0500

    updating devcontainer and exacmple c++ script

--- examples/torch_example.cpp
+++ examples/torch_example.cpp
@@ -1,14 +1,41 @@
 #include <torch/torch.h>
+#include <torch/script.h> 
 #include <iostream>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
 
 using namespace std;
+using namespace cv;
 
 int main() {
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << tensor << std::endl;
+  // torch::Tensor tensor = torch::rand({2, 3});
+  // std::cout << tensor << std::endl;
 
 
-  // torch hub load 
-  string REPO_NAME = "facebookresearch/dinov2"
-  string MODEL_NAME = "dinov2_vitb14_reg"
+
+
+  // Load the TorchScript model
+  torch::jit::script::Module module;
+  
+    try {
+        module = torch::jit::load("/workspaces/HyperTools/dino_vitb16_model.pt");
+    }
+    catch (const c10::Error& e) {
+        std::cerr << "Error loading the model\n";
+        return -1;
+    }
+
+
+   Mat test_img =  imread("../images/lena3.png");
+   // resize to 224, 224
+    cv::resize(test_img,test_img,Size(224,224),INTER_LINEAR); 
+
+
+   imshow("test", test_img);
+
+   waitKey();
+
+
+    std::cout << "Model loaded\n";
 }
